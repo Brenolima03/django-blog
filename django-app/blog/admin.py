f58bd3a22263ad397af2b9af5110c4210149afae
Brenolima03 <brenocslima@outlook.com>
2024-11-02 15:16:38 -0400

    Add related_name for SiteSetup model to prevent conflicts in reverse relationships
    - Added specific related_name to SiteSetup fields like created_by and updated_by.
    - This avoids ambiguity in reverse queries on user instances (e.g., user.post_set.all()) where multiple models reference User.

--- django-app/blog/admin.py
+++ django-app/blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from blog.models import Category, Tag, Page
+from django.forms import ModelForm
+from django.http import HttpRequest
+from blog.models import Category, Post, Tag, Page
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -35,3 +37,25 @@ class PageAdmin(admin.ModelAdmin):
   prepopulated_fields = {
     "slug": ("title",),
   }
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = "id", "title", "is_published",  "created_by",
+  list_display_links = "title",
+  search_fields = "id", "slug", "title", "excerpt", "content",
+  list_per_page = 50
+  list_filter = "category", "is_published",
+  list_editable = "is_published",
+  ordering = "-id",
+  readonly_fields = "created_at", "updated_at", "created_by", "updated_by",
+  prepopulated_fields = {
+    "slug": ("title",),
+  }
+  autocomplete_fields = "tags", "category",
+
+  def save_model(self, request, obj, form, change):
+    if change:
+      obj.updated_by = request.user
+    else:
+      obj.created_by = request.user
+    obj.save()
--- django-app/site_setup/models.py
+++ django-app/site_setup/models.py
@@ -4,14 +4,15 @@ from utils.images import resize_image
 
 class MenuLink(models.Model):
   class Meta:
-    verbose_name = 'Menu Link'
-    verbose_name_plural = 'Menu Links'
+    verbose_name = "Menu Link"
+    verbose_name_plural = "Menu Links"
 
   text = models.CharField(max_length=50)
   url_or_path = models.CharField(max_length=2048)
   new_tab = models.BooleanField(default=False)
   site_setup = models.ForeignKey(
-    'SiteSetup', on_delete=models.CASCADE, blank=True, null=True, default=None
+    "SiteSetup", on_delete=models.CASCADE, blank=True,
+    null=True, default=None, related_name="menu"
   )
 
   def __str__(self):
@@ -19,8 +20,8 @@ class MenuLink(models.Model):
 
 class SiteSetup(models.Model):
   class Meta:
-    verbose_name = 'Setup'
-    verbose_name_plural = 'Setup'
+    verbose_name = "Setup"
+    verbose_name_plural = "Setup"
 
   title = models.CharField(max_length=65)
   description = models.CharField(max_length=255)
@@ -31,9 +32,9 @@ class SiteSetup(models.Model):
   show_pagination = models.BooleanField(default=True)
   show_footer = models.BooleanField(default=True)
   favicon = models.ImageField(
-    upload_to='assets/favicon/%Y/%m/',
+    upload_to="assets/favicon/%Y/%m/",
     blank=True,
-    default='',
+    default="",
     validators=[validate_png]
   )
 
