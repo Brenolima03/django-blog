3970a84a41ec0b2f091a2a0916659ea68bb2f330
Brenolima03 <brenocslima@outlook.com>
2024-11-06 00:40:47 -0400

    route and view for tags

--- django-app/blog/urls.py
+++ django-app/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from blog.views import category, index, page, post, created_by
+from blog.views import category, index, page, post, created_by, tag
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-  path('', index, name='index'),
-  path('post/<slug:slug>/', post, name='post'),
-  path('page/', page, name='page'),
-  path('created_by/<int:author_pk>/', created_by, name='created_by'),
-  path('created_by/<slug:slug>/', category, name='category'),
+  path("", index, name="index"),
+  path("post/<slug:slug>/", post, name="post"),
+  path("page/", page, name="page"),
+  path("created_by/<int:author_pk>/", created_by, name="created_by"),
+  path("category/<slug:slug>/", category, name="category"),
+  path("tag/<slug:slug>/", tag, name="tag"),
 ]
